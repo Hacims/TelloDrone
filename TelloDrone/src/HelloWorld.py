a016eafc06c7726de3dfa8de5d01eef75758cd21
hacim <hacim@192.168.2.13>
2019-07-18 12:04:20 -0500

    added this code

---
+++ TelloDrone/src/HelloWorld.py
@@ -0,0 +1,12 @@
+'''
+Created on Jul 18, 2019
+
+@author: hacim
+'''
+import sys
+print ("Hello World")
+input('input:')
+print ("Lovely to meet you, how are you doing?")
+input('input:')
+print ("Well Have A Great Day")
+sys.exit()
